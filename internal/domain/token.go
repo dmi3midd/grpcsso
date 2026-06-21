056108792826c51c8323e0b580c79a18515c6eb6
dmi3midd <dmi3midd@gmail.com>
2026-06-21 12:39:51 +0300

    feat: implement token repository and add updated_at field to token model.

--- internal/domain/token.go
+++ internal/domain/token.go
@@ -9,4 +9,5 @@ type Token struct {
 	ClientId     string    `json:"clientId" db:"client_id"`
 	ExpiresAt    time.Time `json:"expiresAt" db:"expires_at"`
 	CreatedAt    time.Time `json:"createdAt" db:"created_at"`
+	UpdatedAt    time.Time `json:"updatedAt" db:"updated_at"`
 }
