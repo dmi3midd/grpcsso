6b19651e2bda65ade7c77c31ebcb47365276f45e
dmi3midd <dmi3midd@gmail.com>
2026-06-23 13:14:58 +0300

    feat: implement permission service.

---
+++ internal/service/permission.go
@@ -0,0 +1,150 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/dmi3midd/grpcsso/internal/domain"
+	"github.com/dmi3midd/grpcsso/internal/repository"
+	"github.com/rs/xid"
+)
+
+var (
+	ErrPermissionNotFound = errors.New("permission not found")
+	ErrEmptyPermissions   = errors.New("empty permissions")
+	ErrTooManyPermissions = errors.New("too many permissions")
+)
+
+type PermissionService interface {
+	// GetPermissions returns permissions for a user and client.
+	// Returns an empty slice if the user has no permissions.
+	GetPermissions(ctx context.Context, userId, clientId string) ([]string, error)
+	// SetPermissions overwrites all permissions for a user and client.
+	// Returns [ErrEmptyPermissions] if the permissions slice is empty.
+	// Returns [`ErrTooManyPermissions`] if the permissions slice is too large (more than 16).
+	SetPermissions(ctx context.Context, userId, clientId string, permissions []string) error
+	// HasPermissions checks if a user has ALL the specified permissions.
+	// Returns [ErrEmptyPermissions] if the permissions slice is empty.
+	// Returns ErrTooManyPermissions if the permissions slice is too large (more than 16).
+	HasPermissions(ctx context.Context, userId, clientId string, permissions []string) (bool, error)
+}
+
+type permissionService struct {
+	permissionStore repository.PermissionRepository
+	// userStore       repository.UserRepository
+}
+
+func NewPermissionService(permissionStore repository.PermissionRepository) PermissionService {
+	return &permissionService{
+		permissionStore: permissionStore,
+	}
+}
+
+func (s *permissionService) GetPermissions(ctx context.Context, userId string, clientId string) ([]string, error) {
+	op := "PermissionService.GetPermissions"
+
+	// _, err := s.userStore.GetById(ctx, userId)
+	// if errors.Is(err, repository.ErrUserNotFound) {
+	// 	return nil, fmt.Errorf("%s: %w", op, ErrPermissionNotFound)
+	// }
+	// if err != nil {
+	// 	return nil, fmt.Errorf("%s: %w", op, err)
+	// }
+
+	permissions, err := s.permissionStore.GetByUserIdAndClientId(ctx, userId, clientId)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	if len(permissions) == 0 {
+		return []string{}, nil
+	}
+	result := make([]string, 0, len(permissions))
+	for _, p := range permissions {
+		result = append(result, p.Permission)
+	}
+
+	return result, nil
+}
+
+func (s *permissionService) SetPermissions(ctx context.Context, userId string, clientId string, permissions []string) error {
+	op := "PermissionService.SetPermissions"
+
+	// _, err := s.userStore.GetById(ctx, userId)
+	// if errors.Is(err, repository.ErrUserNotFound) {
+	// 	return fmt.Errorf("%s: %w", op, ErrPermissionNotFound)
+	// }
+	// if err != nil {
+	// 	return fmt.Errorf("%s: %w", op, err)
+	// }
+
+	if len(permissions) == 0 {
+		return fmt.Errorf("%s: %w", op, ErrEmptyPermissions)
+	}
+	if len(permissions) > 16 {
+		return fmt.Errorf("%s: %w", op, ErrTooManyPermissions)
+	}
+	newPermissions := make([]domain.Permission, len(permissions))
+	for i, permission := range permissions {
+		now := time.Now()
+		newPermissions[i] = domain.Permission{
+			Id:         xid.New().String(),
+			UserId:     userId,
+			ClientId:   clientId,
+			Permission: permission,
+			CreatedAt:  now,
+			UpdatedAt:  now,
+		}
+	}
+
+	if err := s.permissionStore.DeleteAllForUserAndClient(ctx, userId, clientId); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	if err := s.permissionStore.CreateMany(ctx, newPermissions); err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (s *permissionService) HasPermissions(ctx context.Context, userId string, clientId string, permissions []string) (bool, error) {
+	op := "PermissionService.HasPermissions"
+
+	// _, err := s.userStore.GetById(ctx, userId)
+	// if errors.Is(err, repository.ErrUserNotFound) {
+	// 	return false, fmt.Errorf("%s: %w", op, ErrPermissionNotFound)
+	// }
+	// if err != nil {
+	// 	return false, fmt.Errorf("%s: %w", op, err)
+	// }
+
+	if len(permissions) == 0 {
+		return false, fmt.Errorf("%s: %w", op, ErrEmptyPermissions)
+	}
+	if len(permissions) > 16 {
+		return false, fmt.Errorf("%s: %w", op, ErrTooManyPermissions)
+	}
+
+	permissionsFromDb, err := s.permissionStore.GetByUserIdAndClientId(ctx, userId, clientId)
+	if err != nil {
+		return false, fmt.Errorf("%s: %w", op, err)
+	}
+	if len(permissionsFromDb) == 0 {
+		return false, nil
+	}
+
+	permSet := make(map[string]bool, len(permissions))
+	for _, p := range permissions {
+		permSet[p] = true
+	}
+
+	for _, p := range permissionsFromDb {
+		if _, ok := permSet[p.Permission]; !ok {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
