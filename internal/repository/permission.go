29bc51ff05ae0bf9406334283489359874ed5b0d
dmi3midd <dmi3midd@gmail.com>
2026-06-23 13:10:22 +0300

    refactor: change GetByUserIdAndClientId signature.

--- internal/repository/permission.go
+++ internal/repository/permission.go
@@ -10,7 +10,7 @@ import (
 
 type PermissionRepository interface {
 	// GetByUserIdAndClientId retrieves all permissions for a user for a specific client.
-	GetByUserIdAndClientId(ctx context.Context, userId, clientId string) ([]string, error)
+	GetByUserIdAndClientId(ctx context.Context, userId, clientId string) ([]domain.Permission, error)
 	// CreateMany creates multiple permissions for a user for a specific client.
 	CreateMany(ctx context.Context, permissions []domain.Permission) error
 	// DeleteAllForUserAndClient deletes all permissions for a user for a specific client.
@@ -27,21 +27,18 @@ func NewPermissionRepo(db *sqlx.DB) PermissionRepository {
 	}
 }
 
-func (r *permissionRepository) GetByUserIdAndClientId(ctx context.Context, userId, clientId string) ([]string, error) {
+func (r *permissionRepository) GetByUserIdAndClientId(ctx context.Context, userId, clientId string) ([]domain.Permission, error) {
 	op := "PermissionRepository.GetByUserIdAndClientId"
 	query := `
-	SELECT permission 
+	SELECT id, user_id, client_id, permission, created_at, updated_at
 	FROM permissions 
 	WHERE user_id = $1 AND client_id = $2
 	`
-	var permissions []string
+	var permissions []domain.Permission
 	err := r.db.SelectContext(ctx, &permissions, query, userId, clientId)
 	if err != nil {
-		return []string{}, fmt.Errorf("%s: %w", op, err)
+		return []domain.Permission{}, fmt.Errorf("%s: %w", op, err)
 	}
-	// if permissions == nil {
-	// 	return []string{}, nil
-	// }
 
 	return permissions, nil
 }
