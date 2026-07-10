865d64fbd7beb05cbe28fe698ede9962c261bd17
dmi3midd <dmi3midd@gmail.com>
2026-07-10 23:29:27 +0300

    refactor: rename refresh_token to token.

--- internal/domain/token.go
+++ internal/domain/token.go
@@ -1,6 +1,6 @@
 package domain
 
-type RefreshToken struct {
+type Token struct {
 	ID           string `json:"id"`
 	UserID       string `json:"user_id"`
 	RefreshToken string `json:"refresh_token"`
